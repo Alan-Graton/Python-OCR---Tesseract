50fcd6c38da79a70b460c296db8165f87d64542f
Alan-Graton <graton.alan@gmail.com>
2021-07-27 00:46:59 -0300

    Functions

--- OCR - Estágio/main.py
+++ OCR - Estágio/main.py
@@ -1,9 +1,48 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('OpenCV Logo.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('images/aurebesh.jpg')
 
-cv2.namedWindow('OCR em Python')
-cv2.imshow('OCR em Python', img)
 
-cv2.waitKey()
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+def remove_noise(image):
+    return cv2.medianBlur(image, 5)
+
+
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+
+def dilate(image):
+    kernel = np.ones((5, 5), np.uint8)
+    return cv2.dilate(image, kernel, iterations=1)
+
+
+def erode(image):
+    kernel = np.ones((5, 5), np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPHO_OPEN, kernel)
+
+
+def canny(image):
+    return cv2.Canny(image, 100, 200)
+
+
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(
+        image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
+
+def match_template(image, template):
+    return cv2.match_template(image, template, cv2.TM_CCOEFF_NORMED)
