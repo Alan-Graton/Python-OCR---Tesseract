6292a63b32da1ed3755b54786a924e7bb5c76eb0
Alan-Graton <graton.alan@gmail.com>
2021-07-27 02:43:35 -0300

    Selecionar apenas a Data da Imagem

--- OCR - Estágio/main.py
+++ OCR - Estágio/main.py
@@ -1,27 +1,25 @@
 import re
 import cv2
-import pytesseract
+import pytesseract as tess
 from pytesseract import Output
-from matplotlib import pyplot as plt
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
 IMG_DIR = 'images/'
-image = cv2.imread(IMG_DIR + 'invoice-sample.jpg')
-d = pytesseract.image_to_data(image, output_type=Output.DICT)
-print('DATA KEYS: \n', d.keys())
+PDF_DIR = 'PDFs/'
+
+img = cv2.imread(IMG_DIR + 'invoice-sample.jpg')
+d = tess.image_to_data(img, output_type = Output.DICT)
+keys = list(d.keys())
+
+date_pattern = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\d\d$'
 
 n_boxes = len(d['text'])
 for i in range(n_boxes):
-    # condition to only pick boxes with a confidence > 60%
     if int(float(d['conf'][i])) > 60:
-        (x, y, w, h) = (d['left'][i], d['top']
-                        [i], d['width'][i], d['height'][i])
-        image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 0)
+        if(re.match(date_pattern, d['text'][i])):
+            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+            img = cv2.rectangle(img, (x,y), (x + w, y + h), (0, 255, 0), 0)
 
-b, g, r = cv2.split(image)
-rgb_img = cv2.merge([r, g, b])
-plt.figure(figsize=(16, 12))
-plt.imshow(rgb_img)
-plt.title('SAMPLE INVOICE WITH WORD LEVEL BOXES')
-plt.show()
+cv2.imshow('img', img)
+cv2.waitKey()
